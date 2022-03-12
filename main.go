5b62460b278eb38a0f950ece318da39f9c46c6d5
annihilatorrrr <tanmoyomg7@gmail.com>
2022-03-12 12:47:50 +0530

    Done.

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"runtime"
 	"runtime/debug"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -45,15 +46,21 @@ func main() {
 	debug.SetGCPercent(3)
 	// Init the mux router
 	router := mux.NewRouter().StrictSlash(true)
-
-	// Route handles & endpoints
-	router.HandleFunc("/", homePage)
-	router.HandleFunc("/md2htmlbv2/", mdfunc).Methods("POST")
 	port := os.Getenv("PORT")
 	// serve it
 	log.Println("API Link:", os.Getenv("LINK"))
 	log.Println("Server at", port)
+	server := &http.Server{
+		Addr:         "0.0.0.0:" + port,
+		Handler:      router,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 3 * time.Second,
+		IdleTimeout:  7 * time.Second,
+	}
+	// Route handles & endpoints
+	router.HandleFunc("/", homePage)
+	router.HandleFunc("/md2htmlbv2/", mdfunc).Methods("POST")
 	runtime.GC()
 	debug.FreeOSMemory()
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
+	log.Fatal(server.ListenAndServe())
 }
